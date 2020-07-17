f42f626d6ce2a91968ff5d9fd66aa66b77a4b23d
bilalhadid <hadid0648@gmail.com>
2020-07-18 02:46:13 +0500

    Completed 4A

--- src/component/Main.js
+++ src/component/Main.js
@@ -101,7 +101,6 @@ export const Main = () => {
           <img
             id="red-queen_and_alice_sprite"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
             alt="Alice and the Red Queen running to stay in place."
             ref={redQueen_alice_sprite}
           />
@@ -112,7 +111,6 @@ export const Main = () => {
         <img
           id="palm3"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
           alt=" "
         />
       </div>
@@ -120,13 +118,11 @@ export const Main = () => {
         <img
           id="bush"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
           alt=" "
         />
         <img
           id="w_rook_upright"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
           alt=" "
         />
       </div>
@@ -134,19 +130,16 @@ export const Main = () => {
         <img
           id="r_pawn_upright"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
           alt=" "
         />
         <img
           id="w_rook"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
           alt=" "
         />
         <img
           id="palm1"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
           alt=" "
         />
       </div>
@@ -154,20 +147,17 @@ export const Main = () => {
         <img
           id="r_pawn"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
           alt=" "
         />
 
         <img
           id="r_knight"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
           alt=" "
         />
         <img
           id="palm2"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
-          srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
           alt=" "
         />
       </div>
